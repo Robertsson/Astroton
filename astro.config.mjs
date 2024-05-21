171e292c90a41979a5965e4254cc13b76ee6461d
Jonas Robertsson <jonas.robertsson@icloud.com>
2024-05-21 11:19:47 +0200

    Cookie concent & Counter stats

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import svelte from "@astrojs/svelte";
 import { SITE } from "./src/config";
 import sitemap from "@astrojs/sitemap";
 
+import "vanilla-cookieconsent/dist/cookieconsent.css";
+import * as CookieConsent from "vanilla-cookieconsent";
 
 // https://astro.build/config
 export default defineConfig({
